09a39c717b4d4ea31c2025a5ab5a865fd1ec8af2
Hunter Heidenreich <hunter.scott.heidenreich@gmailcom>
2021-03-18 21:58:35 -0400

    Update size calculation and add a caching of calculation

--- exec/get_size.py
+++ exec/get_size.py
@@ -1,3 +1,5 @@
+import json
+import os
 import re
 # import pandas as pd
 
@@ -49,9 +51,9 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    data_root = args.data
-
     tokenizers = [SpaceTokenizer]
+    data_root = args.data
+    os.makedirs('out/', exist_ok=True)
 
     if args.ds == 'bf':
         dataset = 'BuzzFace/'
@@ -76,97 +78,127 @@ if __name__ == '__main__':
     else:
         raise ValueError(args)
 
-    print_every = 250_000
+    try:
+        all_posts = json.load(open(f'out/{args.ds}_size.json', 'r+'))
+        all_posts['Start datetime'] = datetime.fromtimestamp(all_posts['Start datetime'])
+        all_posts['End datetime'] = datetime.fromtimestamp(all_posts['End datetime'])
 
-    all_posts = {
-        'Posts':           0,
-        'Conversations':   0,
-        'Chars':           0,
-        'Tokens':          defaultdict(int),
-        'Types (Cased)':   defaultdict(set),
-        'Types (Uncased)': defaultdict(set),
-        'Start datetime':  datetime(year=2050, month=1, day=1, hour=1, minute=1, second=1),
-        'End datetime':    datetime(year=1990, month=1, day=1, hour=1, minute=1, second=1),
-    }
+        en_posts = json.load(open(f'out/{args.ds}_size_en.json', 'r+'))
+        en_posts['Start datetime'] = datetime.fromtimestamp(en_posts['Start datetime'])
+        en_posts['End datetime'] = datetime.fromtimestamp(en_posts['End datetime'])
+    except FileNotFoundError:
+        print_every = 250_000
 
-    en_posts = deepcopy(all_posts)
-    for convo in ConvoReader.iter_read(data_root + dataset, cons=cons):
-        all_posts['Conversations'] += 1
-        en_detect = False
+        all_posts = {
+            'Posts':           0,
+            'Conversations':   0,
+            'Chars':           0,
+            'Tokens':          defaultdict(int),
+            'Types (Cased)':   defaultdict(set),
+            'Types (Uncased)': defaultdict(set),
+            'Start datetime':  datetime(year=2050, month=1, day=1, hour=1, minute=1, second=1),
+            'End datetime':    datetime(year=1990, month=1, day=1, hour=1, minute=1, second=1),
+        }
 
-        for post in convo.posts.values():
-            if post.lang is None:
-                res = get_detector().FindLanguage(text=post.text)
-                post.lang = res.language if res.is_reliable else 'und'
+        en_posts = deepcopy(all_posts)
+        for convo in ConvoReader.iter_read(data_root + dataset, cons=cons):
+            all_posts['Conversations'] += 1
+            en_detect = False
 
-            en_bit = post.lang in {'en', 'und'}
-            if en_bit and not en_detect:
-                en_posts['Conversations'] += 1
-                en_detect = True
+            for post in convo.posts.values():
+                if post.lang is None:
+                    res = get_detector().FindLanguage(text=post.text)
+                    post.lang = res.language if res.is_reliable else 'und'
 
-            all_posts['Posts'] += 1
-            if en_bit:
-                en_posts['Posts'] += 1
-            if all_posts['Posts'] % print_every == 0:
-                print(f'Processed {all_posts["Posts"]} posts...')
+                en_bit = post.lang in {'en', 'und'}
+                if en_bit and not en_detect:
+                    en_posts['Conversations'] += 1
+                    en_detect = True
 
-            all_posts['Chars'] += len(post.text)
-            if en_bit:
-                en_posts['Chars'] += len(post.text)
-
-            if post.created_at and post.created_at < all_posts['Start datetime']:
-                all_posts['Start datetime'] = post.created_at
-
-            if post.created_at and en_bit and post.created_at < en_posts['Start datetime']:
-                en_posts['Start datetime'] = post.created_at
-
-            if post.created_at and post.created_at > all_posts['End datetime']:
-                all_posts['End datetime'] = post.created_at
-
-            if post.created_at and en_bit and post.created_at > en_posts['End datetime']:
-                en_posts['End datetime'] = post.created_at
-
-            for tok in tokenizers:
-                ts = tok.split(post.text)
-                ts_ = tok.split(post.text.lower())
-
-                all_posts['Tokens'][tok.NAME] += len(ts)
+                all_posts['Posts'] += 1
                 if en_bit:
-                    en_posts['Tokens'][tok.NAME] += len(ts)
+                    en_posts['Posts'] += 1
+                if all_posts['Posts'] % print_every == 0:
+                    print(f'Processed {all_posts["Posts"]} posts...')
 
-                all_posts['Types (Cased)'][tok.NAME] |= set(ts)
+                all_posts['Chars'] += len(post.text)
                 if en_bit:
-                    en_posts['Types (Cased)'][tok.NAME] |= set(ts)
+                    en_posts['Chars'] += len(post.text)
 
-                all_posts['Types (Uncased)'][tok.NAME] |= set(ts_)
-                if en_bit:
-                    en_posts['Types (Uncased)'][tok.NAME] |= set(ts_)
+                if post.created_at and post.created_at < all_posts['Start datetime']:
+                    all_posts['Start datetime'] = post.created_at
 
-    all_posts['Tokens'] = dict(all_posts['Tokens'])
-    all_posts['Types (Cased)'] = {k: len(v) for k, v in all_posts['Types (Cased)'].items()}
-    all_posts['Types (Uncased)'] = {k: len(v) for k, v in all_posts['Types (Uncased)'].items()}
+                if post.created_at and en_bit and post.created_at < en_posts['Start datetime']:
+                    en_posts['Start datetime'] = post.created_at
 
-    en_posts['Tokens'] = dict(en_posts['Tokens'])
-    en_posts['Types (Cased)'] = {k: len(v) for k, v in en_posts['Types (Cased)'].items()}
-    en_posts['Types (Uncased)'] = {k: len(v) for k, v in en_posts['Types (Uncased)'].items()}
+                if post.created_at and post.created_at > all_posts['End datetime']:
+                    all_posts['End datetime'] = post.created_at
 
-    t = f"""
-            \\hline
-            Posts & & {display_num(all_posts["Posts"])} & {display_num(en_posts["Posts"])} ({100 * en_posts["Posts"]/all_posts["Posts"]:.2f}\\%) \\\\
-            Conversations & & {display_num(all_posts["Conversations"])} & {display_num(en_posts["Conversations"])} ({100 * en_posts["Conversations"]/all_posts["Conversations"]:.2f}\\%) \\\\
-            Chars & & {display_num(all_posts["Chars"])} & {display_num(en_posts["Chars"])} ({100 * en_posts["Chars"]/all_posts["Chars"]:.2f}\\%) \\\\
-            \\hline
-            Start datetime & & {all_posts["Start datetime"]} & {en_posts["Start datetime"]} \\\\
-            End datetime & & {all_posts["End datetime"]} & {en_posts["End datetime"]} \\\\
-            \\hline
-            Tokens & space-separated & {display_num(all_posts["Tokens"][SpaceTokenizer.NAME])} & {display_num(en_posts["Tokens"][SpaceTokenizer.NAME])} ({100 * en_posts["Tokens"][SpaceTokenizer.NAME]/all_posts["Tokens"][SpaceTokenizer.NAME]:.2f}\\%) \\\\
-            Token Types (cased) & space-separated & {display_num(all_posts["Types (Cased)"][SpaceTokenizer.NAME])} & {display_num(en_posts["Types (Cased)"][SpaceTokenizer.NAME])} ({100 * en_posts["Types (Cased)"][SpaceTokenizer.NAME]/all_posts["Types (Cased)"][SpaceTokenizer.NAME]:.2f}\\%) \\\\
-            Token Types (uncased) & space-separated & {display_num(all_posts["Types (Uncased)"][SpaceTokenizer.NAME])} & {display_num(en_posts["Types (Uncased)"][SpaceTokenizer.NAME])} ({100 * en_posts["Types (Uncased)"][SpaceTokenizer.NAME]/all_posts["Types (Uncased)"][SpaceTokenizer.NAME]:.2f}\\%) \\\\
-            \\hline
-    """
+                if post.created_at and en_bit and post.created_at > en_posts['End datetime']:
+                    en_posts['End datetime'] = post.created_at
+
+                for tok in tokenizers:
+                    ts = tok.split(post.text)
+                    ts_ = tok.split(post.text.lower())
+
+                    all_posts['Tokens'][tok.NAME] += len(ts)
+                    if en_bit:
+                        en_posts['Tokens'][tok.NAME] += len(ts)
+
+                    all_posts['Types (Cased)'][tok.NAME] |= set(ts)
+                    if en_bit:
+                        en_posts['Types (Cased)'][tok.NAME] |= set(ts)
+
+                    all_posts['Types (Uncased)'][tok.NAME] |= set(ts_)
+                    if en_bit:
+                        en_posts['Types (Uncased)'][tok.NAME] |= set(ts_)
+
+        all_posts['Tokens'] = dict(all_posts['Tokens'])
+        all_posts['Types (Cased)'] = {k: len(v) for k, v in all_posts['Types (Cased)'].items()}
+        all_posts['Types (Uncased)'] = {k: len(v) for k, v in all_posts['Types (Uncased)'].items()}
+
+        en_posts['Tokens'] = dict(en_posts['Tokens'])
+        en_posts['Types (Cased)'] = {k: len(v) for k, v in en_posts['Types (Cased)'].items()}
+        en_posts['Types (Uncased)'] = {k: len(v) for k, v in en_posts['Types (Uncased)'].items()}
+
+        all_posts['Start datetime'] = all_posts['Start datetime'].timestamp()
+        all_posts['End datetime'] = all_posts['End datetime'].timestamp()
+        json.dump(all_posts, open(f'out/{args.ds}_size.json', 'w+'))
+        all_posts['Start datetime'] = datetime.fromtimestamp(all_posts['Start datetime'])
+        all_posts['End datetime'] = datetime.fromtimestamp(all_posts['End datetime'])
+
+        en_posts['Start datetime'] = en_posts['Start datetime'].timestamp()
+        en_posts['End datetime'] = en_posts['End datetime'].timestamp()
+        json.dump(en_posts, open(f'out/{args.ds}_size_en.json', 'w+'))
+        en_posts['Start datetime'] = datetime.fromtimestamp(en_posts['Start datetime'])
+        en_posts['End datetime'] = datetime.fromtimestamp(en_posts['End datetime'])
+
+    t = f'\t\t\t {args.ds} & '
+    t += f'{display_num(all_posts["Posts"])} & {display_num(all_posts["Conversations"])} & '
+
+    s_str = all_posts["Start datetime"].strftime('%Y/%m/%d')
+    e_str = all_posts["End datetime"].strftime('%Y/%m/%d')
+    delta = all_posts["End datetime"] - all_posts["Start datetime"]
+    years = delta.days / 365
+    t += f'{s_str} & {e_str} & {years:.2f} & '
+
+    t += f'{display_num(all_posts["Chars"])} & {display_num(all_posts["Tokens"][SpaceTokenizer.NAME])} & '
+    t += f'{display_num(all_posts["Types (Uncased)"][SpaceTokenizer.NAME])}/'
+    t += f'{display_num(all_posts["Types (Cased)"][SpaceTokenizer.NAME])} \\\\'
+
+    en = f'\t\t\t{args.ds} (en) & '
+    en += f'{display_num(en_posts["Posts"])} & {display_num(en_posts["Conversations"])} & '
+
+    s_str = en_posts["Start datetime"].strftime('%Y/%m/%d')
+    e_str = en_posts["End datetime"].strftime('%Y/%m/%d')
+    delta = en_posts["End datetime"] - en_posts["Start datetime"]
+    years = delta.days / 365
+    en += f'{s_str} & {e_str} & {years:.2f} & '
+
+    en += f'{display_num(en_posts["Chars"])} & {display_num(en_posts["Tokens"][SpaceTokenizer.NAME])} & '
+    en += f'{display_num(en_posts["Types (Uncased)"][SpaceTokenizer.NAME])}/'
+    en += f'{display_num(en_posts["Types (Cased)"][SpaceTokenizer.NAME])} \\\\'
 
     print(t)
-
-
-
-
+    print()
+    print(en)
