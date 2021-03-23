cc4d942825aafc06f4d7f3985cf593e998d697ec
Hunter Heidenreich <hunter.scott.heidenreich@gmailcom>
2021-03-22 20:41:25 -0400

    Revise general get_size and add full 4chan stat

--- exec/get_size.py
+++ exec/get_size.py
@@ -1,10 +1,7 @@
 import json
 import os
-import re
-# import pandas as pd
 
 from argparse import ArgumentParser
-from collections import defaultdict
 from copy import deepcopy
 from datetime import datetime
 
@@ -29,29 +26,23 @@ def display_num(num):
         return str(int(num)) if num.is_integer() else f'{num:.2f}'
 
 
-class SpaceTokenizer:
-    NAME = 'space-separated'
-
-    @staticmethod
-    def split(s):
-        return re.split(r'\s+', s)
-
-
 if __name__ == '__main__':
-    parser = ArgumentParser('Demo executable of how one might read raw data into conversational format.')
+    parser = ArgumentParser()
     parser.add_argument('--data', dest='data', required=True, type=str, help='General directory data is located in')
     parser.add_argument('--ds', dest='ds', type=str, default='bf',
                         const='bf',
                         nargs='?',
-                        choices=['cmv', 'rd', 'ntt', 'ctq',
-                                 '4chan-news', '4chan-sci', '4chan-his', '4chan-x',
-                                 '4chan-g', '4chan-pol',
-                                 'outlets', 'bf'],
+                        choices=[
+                            'cmv', 'rd',
+                            'ntt', 'ctq',
+                            '4chan-news', '4chan-sci', '4chan-his', '4chan-x', '4chan-g', '4chan-pol',
+                            'outlets', 'bf',
+                            'chan'
+                        ],
                         help='Dataset key in selection')
 
     args = parser.parse_args()
 
-    tokenizers = [SpaceTokenizer]
     data_root = args.data
     os.makedirs('out/', exist_ok=True)
 
@@ -64,6 +55,10 @@ if __name__ == '__main__':
     elif '4chan' in args.ds:
         dataset = args.ds.replace('-', '/') + '/'
         cons = ChanPost
+    elif args.ds == 'chan':
+        dataset = '4chan/*/'
+        cons = ChanPost
+        title = '4Chan'
     elif args.ds == 'ctq':
         dataset = 'CTQuotes/'
         cons = Tweet
@@ -84,7 +79,7 @@ if __name__ == '__main__':
         all_posts['Start datetime'] = datetime.fromtimestamp(all_posts['Start datetime'])
         all_posts['End datetime'] = datetime.fromtimestamp(all_posts['End datetime'])
 
-        en_posts = json.load(open(f'out/{args.ds}_size_en.json', 'r+'))
+        en_posts = json.load(open(f'out/en_und_{args.ds}_size.json', 'r+'))
         en_posts['Start datetime'] = datetime.fromtimestamp(en_posts['Start datetime'])
         en_posts['End datetime'] = datetime.fromtimestamp(en_posts['End datetime'])
     except FileNotFoundError:
@@ -94,10 +89,6 @@ if __name__ == '__main__':
         all_posts = {
             'Posts':           0,
             'Conversations':   0,
-            'Chars':           0,
-            'Tokens':          defaultdict(int),
-            'Types (Cased)':   defaultdict(set),
-            'Types (Uncased)': defaultdict(set),
             'Start datetime':  datetime(year=2050, month=1, day=1, hour=1, minute=1, second=1),
             'End datetime':    min_thresh_dt,
         }
@@ -123,10 +114,6 @@ if __name__ == '__main__':
                 if all_posts['Posts'] % print_every == 0:
                     print(f'Processed {all_posts["Posts"]} posts...')
 
-                all_posts['Chars'] += len(post.text)
-                if en_bit:
-                    en_posts['Chars'] += len(post.text)
-
                 if post.created_at and all_posts['Start datetime'] > post.created_at > min_thresh_dt:
                     all_posts['Start datetime'] = post.created_at
 
@@ -139,30 +126,6 @@ if __name__ == '__main__':
                 if post.created_at and en_bit and post.created_at > en_posts['End datetime']:
                     en_posts['End datetime'] = post.created_at
 
-                for tok in tokenizers:
-                    ts = tok.split(post.text)
-                    ts_ = tok.split(post.text.lower())
-
-                    all_posts['Tokens'][tok.NAME] += len(ts)
-                    if en_bit:
-                        en_posts['Tokens'][tok.NAME] += len(ts)
-
-                    all_posts['Types (Cased)'][tok.NAME] |= set(ts)
-                    if en_bit:
-                        en_posts['Types (Cased)'][tok.NAME] |= set(ts)
-
-                    all_posts['Types (Uncased)'][tok.NAME] |= set(ts_)
-                    if en_bit:
-                        en_posts['Types (Uncased)'][tok.NAME] |= set(ts_)
-
-        all_posts['Tokens'] = dict(all_posts['Tokens'])
-        all_posts['Types (Cased)'] = {k: len(v) for k, v in all_posts['Types (Cased)'].items()}
-        all_posts['Types (Uncased)'] = {k: len(v) for k, v in all_posts['Types (Uncased)'].items()}
-
-        en_posts['Tokens'] = dict(en_posts['Tokens'])
-        en_posts['Types (Cased)'] = {k: len(v) for k, v in en_posts['Types (Cased)'].items()}
-        en_posts['Types (Uncased)'] = {k: len(v) for k, v in en_posts['Types (Uncased)'].items()}
-
         all_posts['Start datetime'] = all_posts['Start datetime'].timestamp()
         all_posts['End datetime'] = all_posts['End datetime'].timestamp()
         json.dump(all_posts, open(f'out/{args.ds}_size.json', 'w+'))
@@ -171,36 +134,18 @@ if __name__ == '__main__':
 
         en_posts['Start datetime'] = en_posts['Start datetime'].timestamp()
         en_posts['End datetime'] = en_posts['End datetime'].timestamp()
-        json.dump(en_posts, open(f'out/{args.ds}_size_en.json', 'w+'))
+        json.dump(en_posts, open(f'out/en_und_{args.ds}_size.json', 'w+'))
         en_posts['Start datetime'] = datetime.fromtimestamp(en_posts['Start datetime'])
         en_posts['End datetime'] = datetime.fromtimestamp(en_posts['End datetime'])
 
-    t = f'\t\t\t {args.ds} & '
-    t += f'{display_num(all_posts["Posts"])} & {display_num(all_posts["Conversations"])} & '
+    t = f'{args.ds} & '
+    t += f'{display_num(all_posts["Posts"])} & {display_num(en_posts["Posts"])} & '
+    t += f'{display_num(all_posts["Conversations"])} & {display_num(en_posts["Conversations"])} & '
 
     s_str = all_posts["Start datetime"].strftime('%Y/%m/%d')
     e_str = all_posts["End datetime"].strftime('%Y/%m/%d')
     delta = all_posts["End datetime"] - all_posts["Start datetime"]
     years = delta.days / 365
-    t += f'{s_str} & {e_str} & {years:.2f} & '
-
-    t += f'{display_num(all_posts["Chars"])} & {display_num(all_posts["Tokens"][SpaceTokenizer.NAME])} & '
-    t += f'{display_num(all_posts["Types (Uncased)"][SpaceTokenizer.NAME])}/'
-    t += f'{display_num(all_posts["Types (Cased)"][SpaceTokenizer.NAME])} \\\\'
-
-    en = f'\t\t\t{args.ds} (en) & '
-    en += f'{display_num(en_posts["Posts"])} & {display_num(en_posts["Conversations"])} & '
-
-    s_str = en_posts["Start datetime"].strftime('%Y/%m/%d')
-    e_str = en_posts["End datetime"].strftime('%Y/%m/%d')
-    delta = en_posts["End datetime"] - en_posts["Start datetime"]
-    years = delta.days / 365
-    en += f'{s_str} & {e_str} & {years:.2f} & '
-
-    en += f'{display_num(en_posts["Chars"])} & {display_num(en_posts["Tokens"][SpaceTokenizer.NAME])} & '
-    en += f'{display_num(en_posts["Types (Uncased)"][SpaceTokenizer.NAME])}/'
-    en += f'{display_num(en_posts["Types (Cased)"][SpaceTokenizer.NAME])} \\\\'
+    t += f'{s_str} & {e_str} & {years:.2f}  \\\\'
 
     print(t)
-    print()
-    print(en)
