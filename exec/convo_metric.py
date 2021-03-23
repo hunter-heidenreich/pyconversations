1f0e51850b0484254aecdde22a31e0fb4a248cd3
Hunter Heidenreich <hunter.scott.heidenreich@gmailcom>
2021-03-23 11:50:20 -0400

    Overhaul metrics for conversations

--- exec/convo_metric.py
+++ exec/convo_metric.py
@@ -1,4 +1,28 @@
+import os
+
+import numpy as np
+import pandas as pd
+
 from argparse import ArgumentParser
+
+from pyconversations.message import *
+from pyconversations.reader import ConvoReader
+
+
+def display_num(num):
+    if num > 1_000_000_000_000:
+        return f"{num / 1_000_000_000_000:.2f} T"
+    elif num > 1_000_000_000:
+        return f"{num / 1_000_000_000:.2f} B"
+    elif num > 1_000_000:
+        return f"{num / 1_000_000:.2f} M"
+    elif num > 1_000:
+        return f"{num / 1_000:.2f} K"
+    else:
+        if type(num) == int:
+            return str(num)
+
+        return str(int(num)) if num.is_integer() else f'{num:.2f}'
 
 
 if __name__ == '__main__':
@@ -16,4 +40,91 @@ if __name__ == '__main__':
                         ],
                         help='Dataset key in selection')
 
-    metrics = {}
+    args = parser.parse_args()
+
+    data_root = args.data
+    os.makedirs('out/convo/', exist_ok=True)
+
+    if args.sel == 'bf':
+        dataset = 'BuzzFace/'
+        cons = FBPost
+        title = 'BuzzFace'
+    elif args.sel == 'outlets':
+        dataset = 'Outlets/'
+        cons = FBPost
+        title = 'Outlets'
+    elif args.sel == 'chan':
+        dataset = '4chan/*/'
+        cons = ChanPost
+        title = '4Chan'
+    elif '4chan' in args.sel:
+        dataset = args.ds.replace('-', '/') + '/'
+        cons = ChanPost
+        title = dataset.replace('4chan', '')
+    elif args.sel == 'ctq':
+        dataset = 'CTQuotes/'
+        cons = Tweet
+        title = 'CTQuotes'
+    elif args.sel == 'ntt':
+        dataset = 'threads/'
+        cons = Tweet
+        title = 'NewsTweet'
+    elif args.sel == 'cmv':
+        dataset = 'Reddit/CMV/'
+        cons = RedditPost
+        title = 'BNC'
+    elif args.sel == 'rd':
+        dataset = 'Reddit/RD_*/'
+        cons = RedditPost
+        title = 'RedditDialog'
+    else:
+        raise ValueError(args)
+
+    cnt = 0
+    print_every = 2_000
+
+    df = []
+    for convo in ConvoReader.iter_read(data_root + dataset, cons=cons):
+        metric = {
+            # general
+            'messages': convo.messages,
+            'connections': convo.connections,
+            'users': convo.users,
+            'duration': convo.duration,
+
+            # text
+            'chars': convo.chars,
+            'tokens': convo.tokens,
+            'types': len(convo.token_types),
+
+            # graph
+            'density': convo.density,
+
+            'avg_degree': np.mean(convo.degree_hist),
+            'std_degree': np.std(convo.degree_hist),
+            'avg_in_degree': np.mean(convo.in_degree_hist),
+            'std_in_degree': np.std(convo.in_degree_hist),
+            'avg_out_degree': np.mean(convo.out_degree_hist),
+            'std_out_degree': np.std(convo.out_degree_hist),
+
+            'avg_depth': np.mean(convo.depths),
+            'std_depth': np.std(convo.depths),
+            'depth': convo.tree_depth,
+            'avg_width': np.mean(convo.widths),
+            'std_width': np.std(convo.widths),
+            'width': convo.tree_width,
+
+            'diameter': convo.diameter,
+            'radius': convo.radius,
+
+            'assortativity': convo.assortativity,
+            'rich_club_coef': convo.rich_club_coefficient
+        }
+        df.append(metric)
+
+        cnt += 1
+        if cnt % print_every == 0:
+            print(f'Processed {cnt} conversations.')
+
+    df = pd.DataFrame(df)
+    df.to_csv(f'out/convo/{args.sel}.csv')
--- src/pyconversations/convo.py
+++ src/pyconversations/convo.py
@@ -3,7 +3,6 @@ from collections import Counter
 from functools import reduce
 
 import networkx as nx
-from networkx.algorithms import approximation
 
 
 class Conversation:
@@ -18,6 +17,7 @@ class Conversation:
         self._posts = posts  # uid -> post object
         self._edges = edges  # uid -> {reply_tos}
         self._stats = {}
+        self._cache = {}
 
     @property
     def posts(self):
@@ -62,6 +62,9 @@ class Conversation:
 
     def _build_graph(self, directed=False):
         if not directed:
+            if 'graph' in self._cache:
+                return self._cache['graph']
+
             graph = nx.Graph()
 
             # add posts as nodes
@@ -73,9 +76,12 @@ class Conversation:
                 for rid in reps:
                     if uid in self._posts and rid in self._posts:
                         graph.add_edge(uid, rid)
-
+            self._cache['graph'] = graph
             return graph
         else:
+            if 'digraph' in self._cache:
+                return self._cache['digraph']
+
             graph = nx.DiGraph()
 
             # add posts as nodes
@@ -87,7 +93,7 @@ class Conversation:
                 for rid in reps:
                     if uid in self._posts and rid in self._posts:
                         graph.add_edge(rid, uid)
-
+            self._cache['digraph'] = graph
             return graph
 
     def segment(self):
@@ -189,225 +195,81 @@ class Conversation:
             self._stats['sources'] = {uid for uid in es if not es[uid]}
             return self._stats['sources']
 
-    def _network_stats(self):
-        graph = self._build_graph()
+    @property
+    def density(self):
+        return nx.density(self._build_graph())
+
+    @property
+    def degree_hist(self):
+        return nx.degree_histogram(self._build_graph())
+
+    @property
+    def in_degree_hist(self):
         digraph = self._build_graph(directed=True)
+        return [digraph.in_degree[n] for n in digraph.nodes]
 
-        self._stats['density'] = nx.density(graph)
-        self._stats['degree_hist'] = nx.degree_histogram(graph)
-        self._stats['in_degree_hist'] = dict(Counter([digraph.in_degree[n] for n in digraph.nodes]))
-        self._stats['out_degree_hist'] = dict(Counter([digraph.out_degree[n] for n in digraph.nodes]))
-        self._stats['max_clique'] = len(approximation.max_clique(graph))
-        self._stats['avg_cluster'] = approximation.average_clustering(graph)
-        self._stats['tree_width'] = approximation.treewidth_min_degree(graph)[0]
+    @property
+    def out_degree_hist(self):
+        digraph = self._build_graph(directed=True)
+        return [digraph.out_degree[n] for n in digraph.nodes]
 
+    @property
+    def depths(self):
+        srcs = self.sources
+        if len(srcs) > 1:
+            raise ValueError
+
+        root = list(srcs)[0]
+        return [nx.shortest_path_length(self._build_graph(), root, uid) for uid in self.posts]
+
+    @property
+    def tree_depth(self):
+        return max(self.depths)
+
+    @property
+    def widths(self):
+        cnts = dict(Counter(self.depths))
+        return [cnts.get(ix, 0) for ix in range(self.tree_depth + 1)]
+
+    @property
+    def tree_width(self):
+        return max(self.widths)
+
+    @property
+    def assortativity(self):
         try:
-            self._stats['assortativity'] = nx.algorithms.assortativity.degree_pearson_correlation_coefficient(graph)
+            return nx.degree_pearson_correlation_coefficient(self._build_graph())
+        except nx.exception.NetworkXError:
+            return None
         except ValueError:
-            self._stats['assortativity'] = None
+            return None
 
-        # centrality metrics
+    @property
+    def diameter(self):
+        return nx.algorithms.diameter(self._build_graph(), e=self.eccentricity)
+
+    @property
+    def radius(self):
+        return nx.algorithms.radius(self._build_graph(), e=self.eccentricity)
+
+    @property
+    def eccentricity(self):
+        if 'eccentricity' not in self._stats:
+            self._stats['eccentricity'] = nx.algorithms.eccentricity(self._build_graph())
+        return self._stats['eccentricity']
+
+    @property
+    def rich_club_coefficient(self):
         try:
-            self._stats['centrality_eigen'] = nx.algorithms.centrality.eigenvector_centrality_numpy(graph)
-        except TypeError:
-            self._stats['centrality_eigen'] = None
-        self._stats['centrality_katz'] = nx.algorithms.centrality.katz_centrality_numpy(graph)
-        self._stats['centrality_closeness'] = nx.algorithms.centrality.closeness_centrality(graph)
-        self._stats['centrality_betweeness'] = nx.algorithms.centrality.betweenness_centrality(graph)
-        try:
-            self._stats['centrality_current_flow_betweeness'] = nx.algorithms.centrality.current_flow_betweenness_centrality(graph)
-        except ZeroDivisionError:
-            self._stats['centrality_current_flow_betweeness'] = None
-        self._stats['centrality_load'] = nx.algorithms.centrality.load_centrality(graph)
-        self._stats['centrality_degree'] = nx.algorithms.centrality.degree_centrality(graph)
-        self._stats['centrality_info'] = nx.algorithms.centrality.information_centrality(graph)
-        self._stats['centrality_harmonic'] = nx.algorithms.centrality.harmonic_centrality(graph)
-        self._stats['centrality_second_order'] = nx.algorithms.centrality.second_order_centrality(graph)
-
-        self._stats['estrada_index'] = nx.algorithms.centrality.estrada_index(graph)
-
-        self._stats['longest_path'] = len(nx.algorithms.dag_longest_path(digraph))
-
-        self._stats['eccentricity'] = nx.algorithms.eccentricity(graph)
-        self._stats['diameter'] = nx.algorithms.diameter(graph, e=self._stats['eccentricity'])
-        self._stats['radius'] = nx.algorithms.radius(graph, e=self._stats['eccentricity'])
-
-        try:
-            self._stats['non_randomness'], self._stats['non_randomness_rel'] = nx.algorithms.non_randomness(graph)
-        except ZeroDivisionError:
-            self._stats['non_randomness'], self._stats['non_randomness_rel'] = None, None
-
-        try:
-            self._stats['wiener_index'] = nx.algorithms.wiener_index(graph)
-            self._stats['closeness_vitality'] = nx.algorithms.closeness_vitality(graph, wiener_index=self._stats['wiener_index'])
-        except nx.exception.NetworkXPointlessConcept:
-            self._stats['wiener_index'] = None
-            self._stats['closeness_vitality'] = None
-
-        self._stats['s_metric'] = nx.algorithms.s_metric(graph, normalized=False)
-
-        try:
-            self._stats['small_sigma'] = nx.algorithms.sigma(graph)
-        except nx.exception.NetworkXError:
-            self._stats['small_sigma'] = None
-
-        try:
-            self._stats['small_omega'] = nx.algorithms.omega(graph)
-        except nx.exception.NetworkXError:
-            self._stats['small_omega'] = None
-
-        self._stats['simrank'] = nx.algorithms.simrank_similarity_numpy(graph)
-
-        try:
-            self._stats['rich_club_coefficient'] = nx.algorithms.rich_club_coefficient(graph)
+            self._stats['rich_club_coefficient'] = nx.algorithms.rich_club_coefficient(self._build_graph())
         except nx.exception.NetworkXError:
             self._stats['rich_club_coefficient'] = None
         except IndexError:
             self._stats['rich_club_coefficient'] = None
+        except nx.exception.NetworkXAlgorithmError:
+            self._stats['rich_club_coefficient'] = None
 
-        try:
-            self._stats['reciprocity'] = nx.algorithms.reciprocity(graph)
-        except nx.exception.NetworkXError:
-            self._stats['reciprocity'] = None
-
-    def _network_prop(self, prop):
-        try:
-            return self._stats[prop]
-        except KeyError:
-            self._network_stats()
-            return self._stats[prop]
-
-    @property
-    def density(self):
-        return self._network_prop('density')
-
-    @property
-    def degree_hist(self):
-        return self._network_prop('degree_hist')
-
-    @property
-    def in_degree_hist(self):
-        return self._network_prop('in_degree_hist')
-
-    @property
-    def out_degree_hist(self):
-        return self._network_prop('out_degree_hist')
-
-    @property
-    def max_clique(self):
-        return self._network_prop('max_clique')
-
-    @property
-    def avg_cluster(self):
-        return self._network_prop('avg_cluster')
-
-    @property
-    def tree_width(self):
-        return self._network_prop('tree_width')
-
-    @property
-    def assortativity(self):
-        return self._network_prop('assortativity')
-
-    @property
-    def centrality_eigen(self):
-        return self._network_prop('centrality_eigen')
-
-    @property
-    def centrality_katz(self):
-        return self._network_prop('centrality_katz')
-
-    @property
-    def centrality_closeness(self):
-        return self._network_prop('centrality_closeness')
-
-    @property
-    def centrality_load(self):
-        return self._network_prop('centrality_load')
-
-    @property
-    def centrality_degree(self):
-        return self._network_prop('centrality_degree')
-
-    @property
-    def centrality_info(self):
-        return self._network_prop('centrality_info')
-
-    @property
-    def centrality_betweeness(self):
-        return self._network_prop('centrality_betweeness')
-
-    @property
-    def centrality_current_flow_betweeness(self):
-        return self._network_prop('centrality_current_flow_betweeness')
-
-    @property
-    def estrada_index(self):
-        return self._network_prop('estrada_index')
-
-    @property
-    def centrality_harmonic(self):
-        return self._network_prop('centrality_harmonic')
-
-    @property
-    def centrality_second_order(self):
-        return self._network_prop('centrality_second_order')
-
-    @property
-    def longest_path(self):
-        return self._network_prop('longest_path')
-
-    @property
-    def diameter(self):
-        return self._network_prop('diameter')
-
-    @property
-    def radius(self):
-        return self._network_prop('radius')
-
-    @property
-    def eccentricity(self):
-        return self._network_prop('eccentricity')
-
-    @property
-    def non_randomness(self):
-        return self._network_prop('non_randomness')
-
-    @property
-    def non_randomness_rel(self):
-        return self._network_prop('non_randomness_rel')
-
-    @property
-    def wiener_index(self):
-        return self._network_prop('wiener_index')
-
-    @property
-    def closeness_vitality(self):
-        return self._network_prop('closeness_vitality')
-
-    @property
-    def s_metric(self):
-        return self._network_prop('s_metric')
-
-    @property
-    def small_sigma(self):
-        return self._network_prop('small_sigma')
-
-    @property
-    def small_omega(self):
-        return self._network_prop('small_omega')
-
-    @property
-    def simrank(self):
-        return self._network_prop('simrank')
-
-    @property
-    def rich_club_coefficient(self):
-        return self._network_prop('rich_club_coefficient')
-
-    @property
-    def reciprocity(self):
-        return self._network_prop('reciprocity')
+        return self._stats['rich_club_coefficient']
 
     def filter(self, by_langs=None, min_chars=1, before=None, after=None, by_tags=None):
         drop = set()
