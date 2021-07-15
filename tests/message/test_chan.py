772f27da08629c35996d246b2c9694d506f7c226
Hunter Heidenreich <hunter.scott.heidenreich@gmailcom>
2021-07-15 17:21:19 -0400

    Null 4chan test updateE

--- tests/message/test_chan.py
+++ tests/message/test_chan.py
@@ -61,7 +61,7 @@ def test_null_post(null_post):
     assert null_post.author is None
     assert null_post.created_at is None
     assert null_post.reply_to == set()
-    assert null_post.platform is None
+    assert null_post.platform == '4chan'
     assert null_post.tags == set()
     assert null_post.lang is None
 
@@ -141,7 +141,7 @@ def test_read_raw_post(mock_raw_post):
     assert post.author == 'Anonymous'
     assert post.created_at == datetime(2286, 11, 20, 12, 46, 39)
     assert post.reply_to == {12345}
-    assert post.platform == '4Chan'
+    assert post.platform == '4chan'
     assert post.tags == set()
     assert post.lang is None
 
