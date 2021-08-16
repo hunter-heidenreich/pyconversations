fix some un-bounded numerical errors in corner cases of feature extraction

src/pyconversations/feature_extraction/harmonic.py
@@ -19,17 +19,35 @@ def resonator_simple(k1, fs, rs):
     N = rs[-1]
     HN = (1 / np.arange(1, N + 1)).sum()
     ravg = N / HN
-    Mavg = sum(fs) * k1
-    theta = np.log10(fs[0] * Mavg / sum(fs)) / np.log10(Mavg)
+
+    if not ravg:
+        return np.zeros_like(rs)
+
+    sum_fs = sum(fs)
+    Mavg = sum_fs * k1
+
+    if Mavg <= 1:
+        return np.zeros_like(rs)
+
+    if not sum_fs:
+        return np.zeros_like(rs)
+
+    theta = np.log10(fs[0] * Mavg / sum_fs) / np.log10(Mavg)
     Navg = (1 - theta) * Mavg
     fhat = ((rs - theta) ** (-theta)) * (1 - (1 + ravg / rs) ** (-Navg / ravg))
+
     return fhat
 
 
 def NLL_simple(x, fs, rs, bounds):
     x = rebound([x], bounds)
     fhat = resonator_simple(*x, fs, rs)
-    return -(fs * np.log10(fhat / sum(fhat))).sum()
+
+    M = sum(fhat)
+    if not M:
+        return 0
+
+    return -(fs * np.log10(fhat / M)).sum()
 
 
 def params_simple(fs, rs, x0):
@@ -54,14 +72,42 @@ def mixing(frequency):
     HN = (1 / np.arange(1, N + 1)).sum()
     ravg = N / HN
     srs = np.array([sizeranks[f] for f in fs])
-
     k1 = params_simple(fs, srs, N / M)
     Mavg = M * float(k1)
+
+    if not ravg:
+        return {
+            'k1':      float(k1),
+            'theta':   float(0),
+            'entropy': float(0),
+            'N_avg':   float(0),
+            'M_avg':   float(Mavg),
+        }
+
+    if Mavg <= 1:
+        return {
+            'k1':      float(k1),
+            'theta':   float(0),
+            'entropy': float(0),
+            'N_avg':   float(0),
+            'M_avg':   float(Mavg),
+        }
+
+    if not M:
+        return {
+            'k1':      float(k1),
+            'theta':   float(0),
+            'entropy': float(0),
+            'N_avg':   float(0),
+            'M_avg':   float(Mavg),
+        }
+
     theta = np.log10(fs[0] * Mavg / M) / np.log10(Mavg)
     Navg = (1 - theta) * Mavg
 
     def _f(rx):
         return ((rx - theta) ** (-theta)) * (1 - (1 + ravg / rx) ** (-Navg / ravg))
+
     fmodel = _f
 
     fhat = fmodel(np.array([sizeranks[f] for f in fs]))

src/pyconversations/feature_extraction/post_in_conv.py
@@ -320,10 +320,19 @@ def avg_token_entropy(post, conv):
     conv_m = sum(convo_dist.values())
     post_m = sum(post_dist.values())
 
-    if not post_m or not conv_m:
+    if not post_m:
         return 0
 
-    entropy = -(np.log([convo_dist[w] / conv_m for w in post_dist]) / (np.log(conv_n) * post_m)).sum()
+    if conv_n < 2:
+        return 0
+
+    if not conv_m:
+        return 0
+
+    numer = np.log([convo_dist[w] / conv_m for w in post_dist])
+    denom = (np.log(conv_n) * post_m)
+    entropy = -(numer / denom).sum()
+
     return entropy
 
 
@@ -346,27 +355,38 @@ def avg_token_entropy_conv(conv_a, conv_b):
     float
         The entropy
     """
+    # Nothing to compare if no posts...
     if not len(conv_a.posts) or not len(conv_b.posts):
         return 0
 
     joint_conv = conv_a + conv_b
+
+    # Nothing to compare if their identical
+    if len(joint_conv.posts) == len(conv_a.posts):
+        return 0
+
     joint_dist = reduce(lambda x, y: x + y,
                         [post_get_all(p, keys={'type_frequency'})['type_frequency'] for p in joint_conv.posts.values()])
 
     joint_n = len(joint_dist)
     joint_m = sum(joint_dist.values())
 
-    if not joint_m or not joint_n:
+    # Nothing to compare if no types or tokens
+    if not joint_m or joint_n < 2:
         return 0
 
     left_dist = reduce(lambda x, y: x + y,
                        [post_get_all(p, keys={'type_frequency'})['type_frequency'] for p in conv_a.posts.values()])
     left_m = sum(left_dist.values())
 
+    # Nothing to compare if no tokens
     if not left_m:
         return 0
 
-    entropy = -(np.log([joint_dist[w] / joint_m for w in left_dist]) / (np.log(joint_n) * left_m)).sum()
+    numer = np.log([joint_dist[w] / joint_m for w in left_dist])
+    denom = (np.log(joint_n) * left_m)
+    entropy = -(numer / denom).sum()
+
     return entropy
 
 